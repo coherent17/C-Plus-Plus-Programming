cfe195af05259a75310d236c774cc348819146b6
coherent <mnb51817@gmail.com>
2022-06-26 00:42:05 +0800

    Number System

---
+++ NumberSystem.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+	int number1 = 15;		//Decimal
+	int number2 = 017;		//Octal
+	int number3 = 0x0F;		//Hexadecimal
+	int number4 = 0b1111;	//Binary
+
+	std::cout << "number1 : " << number1 << std::endl;
+	std::cout << "number2 : " << number2 << std::endl;
+	std::cout << "number3 : " << number3 << std::endl;
+	std::cout << "number4 : " << number4 << std::endl;
+
+	return 0;
+}
