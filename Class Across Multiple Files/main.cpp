75a72995071adcd3ff81e23a18feccd46686053e
coherent <mnb51817@gmail.com>
2022-07-25 23:47:56 +0800

    add pointer to the class

--- Class Across Multiple Files/main.cpp
+++ Class Across Multiple Files/main.cpp
@@ -18,5 +18,16 @@ int main(){
     cout << c1.get_r() << endl;
     cout << c1.get_h() << endl;
     cout << c1.volume() << endl;
+
+    //managing a stack object through pointer
+    Cylinder *c1ptr = &c1;
+    cout << (*c1ptr).volume() << endl;
+    cout << c1ptr->volume() << endl;
+
+    //managing class object through pointer
+    Cylinder *c2 = new Cylinder(11, 20);    //create object on the heap
+    cout << (*c2).volume() << endl;
+    cout << c2->volume() << endl;
+    delete c2;
     return 0;
 }
