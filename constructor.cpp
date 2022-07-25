fb88cbc204e2e031ddf2bc23e43290f6a8928f4c
coherent <mnb51817@gmail.com>
2022-07-25 16:31:35 +0800

    constructor

---
+++ constructor.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+const double PI = 3.1415926535;
+
+class Cylinder{
+    private:
+        double r;
+        double h;
+
+    public:
+    //Constructor: called by compiler to init the class object
+        Cylinder(){
+            r = 2;
+            h = 2;
+        }
+
+        Cylinder(double r_parameter, double h_parameter){
+            r = r_parameter;
+            h = h_parameter;
+        }
+
+        double volume(){
+            return PI * r * r * h;
+        }
+};
+
+int main(){
+    //using Cylinder() constructor to init the object
+    Cylinder c1;
+    cout << c1.volume() << endl;
+
+    //using Cylinder(double r_parameter, double h_parameter) to init the object
+    Cylinder c2(3, 10);
+    cout << c2.volume() << endl;
+
+}
